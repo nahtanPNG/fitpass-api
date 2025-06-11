f3a8804ff31670a28eab090af5774bdbbef16c47
nahtanpng <nathan.ferreiira.dev@gmail.com>
2025-06-11 19:20:05 -0300

    feat<users-use-case>: Add Unit Testing

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,8 +1,8 @@
 import { FastifyRequest, FastifyReply } from "fastify";
 import z from "zod";
-import { RegisterUseCase } from "../use-cases/register";
+import { RegisterUseCase } from "../../use-cases/register";
 import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository";
-import { UserAlreadyExistsError } from "../use-cases/errors/user-already-exists-error";
+import { UserAlreadyExistsError } from "../../use-cases/errors/user-already-exists-error";
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
---
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -0,0 +1,30 @@
+import { Prisma, User } from "@prisma/client";
+import { UsersRepository } from "../users-repository";
+
+export class InMemoryUsersRepository implements UsersRepository {
+  public entries: User[] = [];
+
+  async findByEmail(email: string) {
+    const user = this.entries.find((entry) => entry.email === email);
+
+    if (!user) {
+      return null;
+    }
+
+    return user;
+  }
+
+  async create(data: Prisma.UserCreateInput) {
+    const user = {
+      id: "user-1",
+      name: data.name,
+      email: data.email,
+      password_hash: data.password_hash,
+      created_at: new Date(),
+    };
+
+    this.entries.push(user);
+
+    return user;
+  }
+}
