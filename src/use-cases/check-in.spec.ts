0bcf18de7c088daf47b8f2821094a232ff4ee9e1
nahtanpng <nathan.ferreiira.dev@gmail.com>
2025-06-19 12:23:22 -0300

    feat: create gym use case; add new errors

--- src/use-cases/check-in.spec.ts
+++ src/use-cases/check-in.spec.ts
@@ -3,24 +3,26 @@ import { InMemoryCheckInsRepository } from "@/repositories/in-memory/in-memory-c
 import { CheckInUseCase } from "./check-in";
 import { InMemoryGymsRepository } from "@/repositories/in-memory/in-memory-gyms-repository";
 import { Decimal } from "@prisma/client/runtime/library";
+import { MaxDistanceError } from "./errors/max-distance-error";
+import { MaxNumberOfCheckInsError } from "./errors/max-number-of-check-ins-error";
 
 let checkInsRepository: InMemoryCheckInsRepository;
 let gymsRepository: InMemoryGymsRepository;
 let sut: CheckInUseCase;
 
 describe("Check in Use Case", () => {
-  beforeEach(() => {
+  beforeEach(async () => {
     checkInsRepository = new InMemoryCheckInsRepository();
     gymsRepository = new InMemoryGymsRepository();
     sut = new CheckInUseCase(checkInsRepository, gymsRepository);
 
-    gymsRepository.entries.push({
+    await gymsRepository.create({
       id: "gym-01",
       title: "Trembo Gym",
       description: "",
       phone: "",
-      latitude: new Decimal(-23.7010944),
-      longitude: new Decimal(-46.7697664),
+      latitude: -23.7010944,
+      longitude: -46.7697664,
     });
 
     vi.useFakeTimers();
@@ -62,7 +64,7 @@ describe("Check in Use Case", () => {
         userLatitude: -23.7010944,
         userLongitude: -46.7697664,
       })
-    ).rejects.toBeInstanceOf(Error);
+    ).rejects.toBeInstanceOf(MaxNumberOfCheckInsError);
   });
   it("should be able to check in on diferrent days", async () => {
     vi.setSystemTime(new Date(2025, 4, 13, 8, 0, 0));
@@ -102,6 +104,6 @@ describe("Check in Use Case", () => {
         userLatitude: -23.7010944,
         userLongitude: -46.7697664,
       })
-    ).rejects.toBeInstanceOf(Error);
+    ).rejects.toBeInstanceOf(MaxDistanceError);
   });
 });
---
+++ src/use-cases/create-gym.spec.ts
@@ -0,0 +1,25 @@
+import { expect, describe, it, beforeEach } from "vitest";
+import { InMemoryGymsRepository } from "@/repositories/in-memory/in-memory-gyms-repository";
+import { CreateGymUseCase } from "./create-gym";
+
+let gymsRepository: InMemoryGymsRepository;
+let sut: CreateGymUseCase;
+
+describe("Register Use Case", () => {
+  beforeEach(() => {
+    gymsRepository = new InMemoryGymsRepository();
+    sut = new CreateGymUseCase(gymsRepository);
+  });
+
+  it("should be able to create gym", async () => {
+    const { gym } = await sut.execute({
+      title: "Trembo Gym",
+      description: null,
+      phone: null,
+      latitude: -23.7010944,
+      longitude: -46.7697664,
+    });
+
+    expect(gym.id).toEqual(expect.any(String));
+  });
+});
